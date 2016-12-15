b17b8a028c986d88b536a4da2c445cb1de03c6c6
miherca <jmjssjd@gmail.com>
2016-12-15 22:49:16 +0100

    Primera version wdiagnos

---
+++ WDIAGNOS.Tests/GenerateSchema_fixture.cs
@@ -0,0 +1,22 @@
+﻿using WDIAGNOS.domain;
+
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace WDIAGNOS.Tests
+{
+    [TestFixture]
+    public class GenerateSchema_Fixture
+    {
+        [Test]
+        public void Can_generate_schema()
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Paciente).Assembly);
+
+            //new SchemaExport(cfg).Execute( false, true, false, false);
+        }
+    }
+}
---
+++ WDIAGNOS.Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WDIAGNOS.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateSchema_Fixture prueba = new GenerateSchema_Fixture();
+            prueba.Can_generate_schema();
+        }
+    }
+}
